39a2467a39d18ff44e54cd8fa1dfcaf4e6c4b350
kenken1107 <64597862+kenken1107@users.noreply.github.com>
2022-04-09 17:16:50 +0900

    initial commit before my first submission

---
+++ README.md
@@ -0,0 +1,17 @@
+# Weather-Journal App Project
+
+## Overview
+This project demonstrates how to create asynchronous web application with node.js and some interactive functionalies using pure Jacascript where the user can get wheather information on given location（zipcode）. The below picture is the expected result after user's inputting.
+
+## Expected Output
+![output](./expected_output.png)
+
+## Instructions
+This will require modifying the `server.js` file and the `website/app.js` file. You can see `index.html` for element references, and once you are finished with the project steps, you can use `style.css` to style your application to customized perfection.
+
+## HOW TO USE THIS APP
+The user only need to:
+
+- Enter a valid zip code, followed by a comma(with no space) then country code (all lowercased) in the input area. (e.g. 166-0012,jp, 94040,us)
+- Write a feeling in the textarea. For example "Its chilly outside"
+- Hit then "Generate" button to get the weather information and feeling.
---
+++ website/app.js
@@ -0,0 +1,98 @@
+/* Global Variables */
+const personalKEY = 'd5fe94d6a3ea6b6749240e38bf650ca6'
+const apiKey      = `&APPID=${personalKEY}&units=imperial`;
+const baseURL    = "http://api.openweathermap.org/data/2.5/weather?zip="
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// "http://api.openweathermap.org/data/2.5/weather?zip=166-0012,JP&APPID=d5fe94d6a3ea6b6749240e38bf650ca6&units=imperial"
+
+// Add event listener for activating click event
+const generateElement = document.getElementById("generate");
+generateElement.addEventListener("click", (event)=>{
+    event.preventDefault();
+    
+    // Get Wheather Data
+    const zipElement   = document.getElementById("zip");
+    const zipcode      = zipElement.value.trim();
+    const url          = baseURL + zipcode + apiKey;
+    getWeatherInfo(url) //← This returns Promise!
+        .then((weatherData)=>{
+            const response = postData('/post', data={
+                temp: weatherData.main.temp,
+                date: newDate,
+                feelings: document.getElementById('feelings').value.trim()});
+            }).then((response)=>{
+                updateUI()
+            }).catch((error)=>{
+                console.log(error);
+            });
+        });
+
+/*                  */
+/* Helper Functions */
+/*                  */
+
+// Async functions for getting weather information (which means it returns Promise)
+const getWeatherInfo = async (url='') => {
+    try { 
+        const response     = await fetch(url);
+        const jsonResponse = await response.json();
+        return jsonResponse
+    }
+    catch(error){
+        console.log(error);
+    }
+};
+
+// Async functions for just posting weather information in 'projectData' object
+const postData = async(url='', data={}) => {
+    try{
+        const response = await fetch(url, {
+                method: 'POST', 
+                credentials: 'same-origin',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(data)
+            }
+        );
+    } catch(error){
+        console.log(error);
+    }
+};
+
+const updateUI = async () => {
+    const request = await fetch('/all');
+
+    try {
+        // Transform into JSON
+        const allData = await request.json()
+        const dataDisplayed = await allData
+        console.log(allData)
+        // Write updated data to DOM elements
+        document.getElementById('temp').innerHTML    = Math.round(dataDisplayed.temp)+ 'degrees';
+        document.getElementById('content').innerHTML = dataDisplayed.feelings;
+        document.getElementById("date").innerHTML    = dataDisplayed.date;
+    }
+    catch(error) {
+        console.log("error", error);
+        // appropriately handle the error
+    }
+};
+
+// const updateUI = async() => {
+//     const request = await fetch('/all');
+//     try {
+//         const allData = await request.json();
+//         console.log(allData);
+//         // update new entry values
+//         if (allData.date !== undefined && allData.temp !== undefined && allData.content !== undefined) {
+//             document.getElementById('date').innerHTML = allData.date;
+//             document.getElementById('temp').innerHTML = allData.temp + ' degree C';
+//             document.getElementById('content').innerHTML = allData.content;
+//         }
+//     } catch (error) {
+//         console.log('error', error);
+//     }
+// };
